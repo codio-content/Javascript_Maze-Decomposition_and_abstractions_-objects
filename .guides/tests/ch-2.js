47147322c671be1e10c730bac0d1ff1ec50f404c
SeanPackham <sean@seanpackham.com>
2015-06-18 12:40:35 +0000

    finished tests

--- .guides/tests/ch-2.js
+++ .guides/tests/ch-2.js
@@ -1,8 +1,68 @@
 
+function checkWall(where) {
+  var x = player.x
+  var y = player.y
+  var hit = false
+
+  switch (where.toUpperCase()) {
+    case 'L':
+      if ( tiles[x-1][y].type == 'wall') {
+        hit = true
+      }
+      break
+      case 'R':
+      if ( tiles[x+1][y].type == 'wall') {
+        hit = true
+      }
+      break
+      case 'A':
+      if ( tiles[x][y-1].type == 'wall') {
+        hit = true
+      }
+      break
+      case 'B':
+      if ( tiles[x][y+1].type == 'wall') {
+        hit = true
+      }
+      break
+  }
+  return hit
+} 
+
 $.getScript(window.location.origin + '/public/js/' + window.testEnv.cmd + '.js?_=' + Date.now())
 .done(function (script, status) {
+             
+  keyPressed (function (keyCode) {
+    keyPressedEvent(keyCode)       
+  })        
+  createEmptyMaze(12, 8)
+  addWall(1,1)
+  addWall(4,2)
+  addWall(6,2)
+  addWall(5,5)
+  addGoal()
+  addPlayer()
+
+  var goalFound = false;
+  
+  // don't need to flush_stack to server
+  window._flushStack = function() {
+    goalFound = true;
+  }
+  
+  for(var i = 0; i < 100; i++) {
+    resetKeys();
+    // C
+    keyevent({ which: 67, type: 'keydown' });    
+    update()
     
-  codio.setButtonValue(window.testEnv.id, codio.BUTTON_STATE.INVALID, "Test not implemented yet. Coming soon.");
+    if(goalFound) {
+      return codio.setButtonValue(window.testEnv.id, codio.BUTTON_STATE.SUCCESS, 'Well done!');      
+    }
+  
+  }      
+
+  codio.setButtonValue(window.testEnv.id, codio.BUTTON_STATE.FAILURE, 'Not quite right, try again!');
   
 })
 .fail(function (jqxhr, settings, exception) {
--- public/js/ch-2.js
+++ public/js/ch-2.js
@@ -1,16 +1,7 @@
 
 function keyPressedEvent(keyCode) {
 
-  if( !checkWall('A') )
-    player.moveUp()
-  else if( !checkWall('R') )
-    player.moveRight()
-  else if( !checkWall('L') )
-    player.moveLeft()
-  else if( !checkWall('B') )
-    player.moveDown()
+
 
 }
-
-
 
