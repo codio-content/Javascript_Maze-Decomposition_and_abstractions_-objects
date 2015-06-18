e80268a1ff438923aa304fd469ee070651ed505d
SeanPackham <sean@seanpackham.com>
2015-06-18 12:24:08 +0000

    first test

--- .guides/tests/ch-1.js
+++ .guides/tests/ch-1.js
@@ -1,8 +1,46 @@
 
 $.getScript(window.location.origin + '/public/js/' + window.testEnv.cmd + '.js?_=' + Date.now())
 .done(function (script, status) {
-    
-  codio.setButtonValue(window.testEnv.id, codio.BUTTON_STATE.INVALID, "Test not implemented yet. Coming soon.");
+   
+  keyPressed (function (keyCode) {    
+    if ( keyCode == 'LEFT' ) {
+      player.moveLeft()
+    } 
+    if ( keyCode == 'RIGHT' ) {
+      player.moveRight()
+    } 
+    if ( keyCode == 'UP' ) {
+      player.moveUp()
+    } 
+    if ( keyCode == 'DOWN' ) {
+      player.moveDown()
+    }  
+    keyPressedEvent(keyCode)
+
+  })
+  createEmptyMaze(10, 6)
+  addWall(1, 2)
+  addWall(2, 2)
+  addWall(3, 2)
+  addWall(3, 3)
+  addWall(3, 4)
+  addWall(3, 5)
+  addGoal()
+  addPlayer()
+  
+  
+  // fake the keyevent
+  resetKeys();
+  keyevent({ which: 67, type: 'keydown' });
+
+  // one update event to process key event
+  update();
+
+  if(player.x == 5 && player.y == 3) {
+    return codio.setButtonValue(window.testEnv.id, codio.BUTTON_STATE.SUCCESS, 'Well done!');      
+  }
+  
+  codio.setButtonValue(window.testEnv.id, codio.BUTTON_STATE.FAILURE, 'Not quite right, try again!');
   
 })
 .fail(function (jqxhr, settings, exception) {
--- public/js/ch-1.js
+++ public/js/ch-1.js
@@ -2,7 +2,8 @@
 function keyPressedEvent(keyCode) {
 
   if (keyCode == 'C') {
-
+player.x = 5
+    player.y = 3
   }
   
 }
